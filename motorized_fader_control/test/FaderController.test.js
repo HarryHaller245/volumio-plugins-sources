f5eeb8cd6f66abbdcede4f29ead0e3a0cd055415
HarryHaller245 <karim.jedlitschka@gmail.com>
2024-07-06 01:45:33 +0200

    Add fader move functionality with speed and FaderMove class

--- motorized_fader_control/test/FaderController.test.js
+++ motorized_fader_control/test/FaderController.test.js
@@ -3,25 +3,35 @@ const { FaderController, FaderMove } = require('../lib/FaderController');
 
 const fader_count = 2;
 
-const messageRateLimit = 10; //! produces mybe crashes with a limit below 5 ?
+
+const messageDelay = 0; //! produces maybe crashes with a limit below 5 ?
 const MIDILog = false;
-const speeds = [80, 50, 10];
+const ValueLog = false;
+
+const speedHigh = 30;
+const speedMedium = 20;
+const speedLow = 10;
+const speeds = [speedHigh, speedMedium, speedLow];
+
+jest.setTimeout(30000); // 30 seconds
 
 describe('FaderController', () => {
   let faderController;
 
   beforeAll(done => {
-    faderController = new FaderController(undefined, fader_count, messageRateLimit, MIDILog, speeds);
+    faderController = new FaderController(undefined, fader_count, messageDelay, MIDILog, speeds, ValueLog);
     faderController.setupSerial('/dev/ttyUSB0', 1000000);
   
     // Add a delay before starting the FaderController
-    setTimeout(async () => {
-      faderController.start();
-      done();
-    }, 3000);
+    new Promise(resolve => setTimeout(resolve, 5000))
+      .then(async () => {
+        await faderController.start();
+        done();
+      });
   });
 
   afterAll(async () => {
+
     // Then stop the controller
     await faderController.stop();
     faderController.closeSerial();
@@ -34,10 +44,14 @@ describe('FaderController', () => {
   test('FaderController tests movement', async () => {
     //test 1: move faders to a position
     const indexes = [0,1];
-    const progression = 100;
+    const progression = 70;
 
-    const moveA = new FaderMove(indexes, 100, 10); 
-    await faderController.move_faders(moveA, false);
+    const moveA = new FaderMove(indexes, progression, 10); 
+    await faderController.moveFaders(moveA, false);
+    expect(faderController.getFaderProgressions(indexes)).toEqual([progression, progression]);
 
-  }, 5000);
+    //test 4: Speed Calibration
+    await faderController.calibrateSpeeds(indexes, 0, 100, 20, 1, 20);
+    
+  }, 50000);
 });
