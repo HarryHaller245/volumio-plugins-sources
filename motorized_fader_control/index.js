1eb7ab2229d3dcb2b9134ca4224fd0d51aa31feb
HarryHaller245 <karim.jedlitschka@gmail.com>
2024-09-08 12:05:16 +0200

    fix

--- motorized_fader_control/index.js
+++ motorized_fader_control/index.js
@@ -1,80 +1,65 @@
 'use strict';
 
 var libQ = require('kew');
-var fs=require('fs-extra');
+var fs = require('fs-extra');
 var config = new (require('v-conf'))();
 var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
-
-
-module.exports = motorizedFaderControl;
-function motorizedFaderControl(context) {
-	var self = this;
-
-	this.context = context;
-	this.commandRouter = this.context.coreCommand;
-	this.logger = this.context.logger;
-	this.configManager = this.context.configManager;
-
-}
-
-
 const { FaderController, FaderMove } = require('../lib/FaderController');
 
-// Use fader and midiParser in your plugin
+module.exports = motorizedFaderControl;
 
-//start function
+function motorizedFaderControl(context) {
+    var self = this;
+
+    this.context = context;
+    this.commandRouter = this.context.coreCommand;
+    this.logger = this.context.logger;
+    this.configManager = this.context.configManager;
+
+    // Initialize FaderController
+    this.faderController = null;
+
+	//caches
 
 
-//stop function
-
-
-
-
-
-motorizedFaderControl.prototype.onVolumioStart = function()
-{
-	var self = this;
-	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
-	this.config = new (require('v-conf'))();
-	this.config.loadFile(configFile);
+motorizedFaderControl.prototype.onVolumioStart = function() {
+    var self = this;
+    var configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
+    this.config = new (require('v-conf'))();
+    this.config.loadFile(configFile);
 
     return libQ.resolve();
 };
 
 motorizedFaderControl.prototype.onStart = function() {
     var self = this;
-	var defer=libQ.defer();
+    var defer = libQ.defer();
 
-
-    this.faderController = new FaderController(this.logger, faderCount, messageDelay, MIDILog, speeds, ValueLog, MoveLog);
-    this.faderController.setupSerial('/dev/ttyUSB0', 1000000);
-
-    // Start FaderController
-    new Promise(resolve => setTimeout(resolve, 5000))
-        .then(async () => {
-            await self.faderController.start(false);
-            self.faderController.setFaderProgressionMap(undefined, trimMap);
-            defer.resolve();
+    // connect to websocket ?
+    // connect onPushState callbacks ?
+    // Initialize FaderController with configurations
 
     return defer.promise;
 };
 
 motorizedFaderControl.prototype.onStop = function() {
     var self = this;
-    var defer=libQ.defer();
+    var defer = libQ.defer();
 
-    // Once the Plugin has successfull stopped resolve the promise
-    defer.resolve();
+    // Stop FaderController
+    self.faderController.stop().then(() => {
+        self.faderController.closeSerial();
+        defer.resolve();
+    });
 
-    return libQ.resolve();
+    return defer.promise;
 };
 
 motorizedFaderControl.prototype.onRestart = function() {
     var self = this;
     // Optional, use if you need it
 };
-
 
 // Configuration Methods -----------------------------------------------------------------------------
 
@@ -84,17 +69,13 @@ motorizedFaderControl.prototype.getUIConfig = function() {
 
     var lang_code = this.commandRouter.sharedVars.get('language_code');
 
-    self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
-        __dirname+'/i18n/strings_en.json',
+    self.commandRouter.i18nJson(__dirname + '/i18n/strings_' + lang_code + '.json',
+        __dirname + '/i18n/strings_en.json',
         __dirname + '/UIConfig.json')
-        .then(function(uiconf)
-        {
-
-
+        .then(function(uiconf) {
             defer.resolve(uiconf);
         })
-        .fail(function()
-        {
+        .fail(function() {
             defer.reject(new Error());
         });
 
@@ -102,185 +83,201 @@ motorizedFaderControl.prototype.getUIConfig = function() {
 };
 
 motorizedFaderControl.prototype.getConfigurationFiles = function() {
-	return ['config.json'];
+    return ['config.json'];
 }
 
 motorizedFaderControl.prototype.setUIConfig = function(data) {
-	var self = this;
-	//Perform your installation tasks here
+    var self = this;
+    // Perform your installation tasks here
 };
 
 motorizedFaderControl.prototype.getConf = function(varName) {
-	var self = this;
-	//Perform your installation tasks here
+    var self = this;
+    // Perform your installation tasks here
 };
 
 motorizedFaderControl.prototype.setConf = function(varName, varValue) {
-	var self = this;
-	//Perform your installation tasks here
+    var self = this;
+    // Perform your installation tasks here
 };
-
-
 
 // Playback Controls ---------------------------------------------------------------------------------------
-// If your plugin is not a music_sevice don't use this part and delete it
 
-
-motorizedFaderControl.prototype.addToBrowseSources = function () {
-
-	// Use this function to add your music service plugin to music sources
-    //var data = {name: 'Spotify', uri: 'spotify',plugin_type:'music_service',plugin_name:'spop'};
-    this.commandRouter.volumioAddToBrowseSources(data);
-};
-
-motorizedFaderControl.prototype.handleBrowseUri = function (curUri) {
-    var self = this;
-
-    //self.commandRouter.logger.info(curUri);
-    var response;
-
-
-    return response;
-};
-
-
-
-// Define a method to clear, add, and play an array of tracks
 motorizedFaderControl.prototype.clearAddPlayTrack = function(track) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::clearAddPlayTrack');
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::clearAddPlayTrack');
 
-	self.commandRouter.logger.info(JSON.stringify(track));
+    self.commandRouter.logger.info(JSON.stringify(track));
 
-	return self.sendSpopCommand('uplay', [track.uri]);
+    return self.sendSpopCommand('uplay', [track.uri]);
 };
 
-motorizedFaderControl.prototype.seek = function (timepos) {
+motorizedFaderControl.prototype.seek = function(timepos) {
     this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::seek to ' + timepos);
 
-    return this.sendSpopCommand('seek '+timepos, []);
+    return this.sendSpopCommand('seek ' + timepos, []);
 };
 
 // Stop
 motorizedFaderControl.prototype.stop = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::stop');
-
-
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::stop');
+    // Add fader stop logic here
 };
 
 // Spop pause
 motorizedFaderControl.prototype.pause = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::pause');
-
-
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::pause');
+    // Add fader pause logic here
 };
 
 // Get state
 motorizedFaderControl.prototype.getState = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::getState');
-
-
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::getState');
+    
 };
 
 //Parse state
-motorizedFaderControl.prototype.parseState = function(sState) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::parseState');
-
-	//Use this method to parse the state and eventually send it with the following function
+motorizedFaderControl.prototype.parseState = function(State) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::parseState');
+    // Use this method to parse the state and eventually send it with the following function
 };
 
 // Announce updated State
 motorizedFaderControl.prototype.pushState = function(state) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::pushState');
+
+    return self.commandRouter.servicePushState(state, self.servicename);
+};
+
+// Event Handling ---------------------------------------------------------------------------------------
+
+motorizedFaderControl.prototype.onPlay = function() {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::onPlay');
+    // Add logic to handle fader movements on play
+};
+
+motorizedFaderControl.prototype.onPause = function() {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::onPause');
+    // Add logic to handle fader movements on pause
+};
+
+motorizedFaderControl.prototype.onStop = function() {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::onStop');
+    // Add logic to handle fader movements on stop
+};
+
+motorizedFaderControl.prototype.onSeek = function(timepos) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::onSeek to ' + timepos);
+    // Add logic to handle fader movements on seek
+};
+
+motorizedFaderControl.prototype.onTrackChanged = function(track) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::onTrackChanged');
+    // Add logic to handle fader movements on track change
+};
+
+
+
+
+// Fader Playback Controls ---------------------------------------------------------------------------------------
+
+motorizedFaderControl.prototype.getVolumioState = function() {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::getVolumioState');
+};
+
+
+/**
+ * Handles the playback trace to determine the correct fader position based on the current playback state.
+ *
+ * This method uses the playback state to calculate the target fader progression. It logs the state and calculates
+ * the target progression based on the current seek position and the total duration of the track.
+ *
+ * @param {Object} state - The current playback state object.
+ * @param {number} state.seek - The current seek position in milliseconds.
+ * @param {number} state.duration - The total duration of the track in seconds.
+ * @returns {number} The target fader progression as a percentage (0-100).
+ */
+motorizedFaderControl.prototype.getPlaybackTrace = function(state) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::getPlaybackTrace');
+
+    try {
+        const currentSeek = state.seek; // Current seek position in milliseconds
+        const totalDurationMs = state.duration * 1000; // Convert duration from seconds to milliseconds
+
+        const targetProgression = (currentSeek / totalDurationMs) * 100;
+        return targetProgression; // This can easily be a float
+    } catch (e) {
+        self.commandRouter.logger.error(e);
+        return 0; // Return a default value in case of error
+    }
+};
+
+motorizedFaderControl.prototype.getPlaybackTraceAlbum = function(state) {
+	//advanced version of the getPlaybackTrace method
+	//this will try to get the current track progression based on the album progression
 	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::pushState');
+	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::handlePlaybackTrace');
+	// add logic to handle fader movements on playback TRACE:
+	// we use the state to determine the correct fader position.
+	//calculate the progression based on the current seek in the current track in the total playlist duration
 
-	return self.commandRouter.servicePushState(state, self.servicename);
-};
+
+}
 
 
-motorizedFaderControl.prototype.explodeUri = function(uri) {
+motorizedFaderControl.prototype.getPlaybackTracePlaylist = function(state) {
+	//advanced version of the getPlaybackTrace method
+	//this will try to get the current track progression based on the playlist progression
 	var self = this;
-	var defer=libQ.defer();
+	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::handlePlaybackTrace');
+	//this will be a bit more difficult
+	//calculate the progression based on the current seek in the current track in the total playlist duration
+	
+}
 
-	// Mandatory: retrieve all info for a given URI
+motorizedFaderControl.prototype.getPlaybackTraceRadio = function(state) {
+	//advanced version of the getPlaybackTrace method
+	//this will try to get the current track progression based on the radio progression
+	var self = this;
+	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::handlePlaybackTrace');
+	//dont know if possible, radio does not have an end time
+}
 
-	return defer.promise;
+motorizedFaderControl.prototype.getPlaybackTraceQueue = function(state) {
+	//advanced version of the getPlaybackTrace method
+	//this will try to get the current track progression based on the queue progression
+	var self = this;
+	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::handlePlaybackTrace');
+	//get the complete queue duration
+	//calculate the progression based on the current seek in the current track in the total queue duration
+}
+
+
+
+
+
+// FADER Touch Callbacks
+// These methods are called when a fader is touched or released.
+motorizedFaderControl.prototype.onFaderTouch = function(faderIndex) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFadezrControl::onFaderTouch');
+    // Handle fader touch logic here
 };
 
-motorizedFaderControl.prototype.getAlbumArt = function (data, path) {
-
-	var artist, album;
-
-	if (data != undefined && data.path != undefined) {
-		path = data.path;
-	}
-
-	var web;
-
-	if (data != undefined && data.artist != undefined) {
-		artist = data.artist;
-		if (data.album != undefined)
-			album = data.album;
-		else album = data.artist;
-
-		web = '?web=' + nodetools.urlEncode(artist) + '/' + nodetools.urlEncode(album) + '/large'
-	}
-
-	var url = '/albumart';
-
-	if (web != undefined)
-		url = url + web;
-
-	if (web != undefined && path != undefined)
-		url = url + '&';
-	else if (path != undefined)
-		url = url + '?';
-
-	if (path != undefined)
-		url = url + 'path=' + nodetools.urlEncode(path);
-
-	return url;
-};
-
-
-
-
-
-motorizedFaderControl.prototype.search = function (query) {
-	var self=this;
-	var defer=libQ.defer();
-
-	// Mandatory, search. You can divide the search in sections using following functions
-
-	return defer.promise;
-};
-
-motorizedFaderControl.prototype._searchArtists = function (results) {
-
-};
-
-motorizedFaderControl.prototype._searchAlbums = function (results) {
-
-};
-
-motorizedFaderControl.prototype._searchPlaylists = function (results) {
-
-
-};
-
-motorizedFaderControl.prototype._searchTracks = function (results) {
-
-};
-
-motorizedFaderControl.prototype.goto=function(data){
-    var self=this
-    var defer=libQ.defer()
-
-// Handle go to artist and go to album function
-
-     return defer.promise;
+motorizedFaderControl.prototype.onFaderRelease = function(faderIndex) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'motorizedFaderControl::onFaderRelease');
+    // Handle fader release logic here
 };
